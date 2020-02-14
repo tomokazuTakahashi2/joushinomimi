fd18464b65d592505715cbc0951e6c47b1c78a62
Tomokazu Takahashi <takahashitomokazu4@gmail.com>
2020-02-14 21:42:05 +0900

    Initial Commit

---
+++ Joushinomimi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Joushinomimi
+//
+//  Created by Raphael on 2020/02/14.
+//  Copyright © 2020 takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
